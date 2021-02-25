4851e7b05752359e988639cf9920172da83cdc61
ccx <1805876407@qq.com>
2021-02-25 04:47:02 -0800

    memoryControl

--- kernel/memory.h
+++ kernel/memory.h
@@ -3,6 +3,17 @@
 #include "../lib/stdint.h"
 #include "../lib/bitmap.h"
 
+enum pool_flags{
+	PF_KERNEL = 1,
+	PF_USER = 2
+};
+
+#define PG_P_1	1
+#define PG_P_0	0
+#define PG_RW_R	0
+#define	PG_RW_W 2
+#define PG_US_S 0
+#define PG_US_U 4
 
 struct virtual_addr{
 	struct bitmap vaddr_bitmap;
@@ -11,4 +22,9 @@ struct virtual_addr{
 
 extern struct pool kernel_pool, user_pool;
 void mem_init(void);
+void* get_kernel_pages(uint32_t pg_cnt);
+void* malloc_page(enum pool_flags pf, uint32_t pg_cnt);
+void malloc_init(void);
+uint32_t* pte_ptr(uint32_t vaddr);
+uint32_t* pde_ptr(uint32_t vaddr);
 #endif
